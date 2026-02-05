708d64f93453db297985615b3ff2f82faca2850e
Furqan Khurrum <furqan.khurrum99@gmail.com>
2026-02-05 12:03:11 -0500

    Delete frontend/.next/dev directory

--- frontend/.next/dev/server/chunks/ssr/_next-internal_server_app_scan-history_page_actions_58c52639.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/scan-history/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_scan-history_page_actions_58c52639.js.map
--- frontend/.next/dev/server/chunks/ssr/_next-internal_server_app_scan-result_page_actions_d28177f1.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/scan-result/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_scan-result_page_actions_d28177f1.js.map
